ff97acf6fdedee3390a386f428ea51a2ba385ef1
Matt Isenhower <matt@isenhower.com>
2022-08-28 18:06:03 -0700

    Improve appearance on mobile

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,9 @@
     <div class="grow flex items-center justify-center">
       <div class="relative w-full max-w-2xl m-4">
         <Splat3 class="absolute w-48 -bottom-16 left-28" fill="fill-splatoon-red" />
-        <Splat2 class="absolute w-64 -top-20 -left-20" fill="fill-splatoon-yellow" />
+        <Splat2 class="absolute w-64 -top-20 -left-20" fill="fill-splatoon-orange" />
         <Splat4 class="absolute w-64 -top-20 -right-20" fill="fill-splatoon-blue" />
-        <Splat1 class="absolute w-48 -bottom-36 right-20" fill="fill-splatoon-green" />
+        <Splat1 class="absolute hidden sm:block w-48 -bottom-36 right-20" fill="fill-splatoon-green" />
 
         <div class="relative">
           <div class="font-splatoon1 text-5xl text-center">Splatoon 3</div>
