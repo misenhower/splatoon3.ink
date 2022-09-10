2e62e4a4c7ee7f9161a1415aefe82e23aaa73a56
Matt Isenhower <matt@isenhower.com>
2022-09-10 13:11:44 -0700

    Add data updaters to the cron jobs

--- app/cron.mjs
+++ app/cron.mjs
@@ -1,6 +1,8 @@
 import { CronJob } from "cron";
+import { updateAll } from "./data/index.mjs";
 import { warmCache } from "./splatnet/index.mjs";
 
 export default function() {
   new CronJob('5,20,35,50 * * * *', warmCache, null, true);
+  new CronJob('10 0 * * * *', updateAll, null, true);
 }
