ea609494c57d2410442f4a04e48bc70adda39fe3
Matt Isenhower <matt@isenhower.com>
2022-09-10 20:32:43 -0700

    Fix dynamic asset handling with Vite

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,28 @@ import { fileURLToPath, URL } from 'url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const redirectToDist = [
+  '/assets/splatnet/',
+  '/data/',
+];
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    {
+      // Quick hack to redirect dynamic assets to the /dist/ directory
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (redirectToDist.some(s => req.url.startsWith(s))) {
+            req.url = '/dist' + req.url;
+          }
+
+          next();
+        });
+      },
+    },
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
