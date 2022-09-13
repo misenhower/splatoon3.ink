a40c74c013d99c9052416c5cc16bb48b624692c6
Matt Isenhower <matt@isenhower.com>
2022-09-12 18:30:00 -0700

    Send tweets from cron job

--- app/cron.mjs
+++ app/cron.mjs
@@ -1,8 +1,12 @@
 import { CronJob } from "cron";
 import { updateAll } from "./data/index.mjs";
 import { warmCache } from "./splatnet/index.mjs";
+import { sendTweets } from "./twitter/index.mjs";
 
 export default function() {
   new CronJob('5,20,35,50 * * * *', warmCache, null, true);
-  new CronJob('10 0 * * * *', updateAll, null, true);
+  new CronJob('10 0 * * * *', async () => {
+    await updateAll();
+    await sendTweets();
+  }, null, true);
 }
