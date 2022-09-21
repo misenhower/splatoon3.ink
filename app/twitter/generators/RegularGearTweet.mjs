c5756663a6a2bacf9a03fad91dacdbf224f605aa
Matt Isenhower <matt@isenhower.com>
2022-09-20 21:19:06 -0700

    Add regular gear tweet

---
+++ app/twitter/generators/RegularGearTweet.mjs
@@ -0,0 +1,36 @@
+import TweetGenerator from "./TweetGenerator.mjs";
+import Media from "../Media.mjs";
+import { useGearStore } from "../../../src/stores/gear.mjs";
+
+export default class RegularGearTweet extends TweetGenerator
+{
+  key = 'gear.regular';
+  name = 'Regular Gear';
+
+  async getLatestGear() {
+    await this.preparePinia();
+
+    return useGearStore().regularGear?.[0];
+  }
+
+  async getDataTime() {
+    let gear = await this.getLatestGear();
+
+    // We only have end times for gear, so use that to track the tweet time
+    return Date.parse(gear.saleEndTime);
+  }
+
+  async _getStatus() {
+    let gear = await this.getLatestGear();
+
+    return `Up now on SplatNet: ${gear.gear.name} with ${gear.gear.primaryGearPower.name} #splatnet3`;
+  }
+
+  /** @param {ScreenshotHelper} screenshotHelper */
+  async _getMedia(screenshotHelper) {
+    let media = new Media;
+    media.file = await screenshotHelper.capture('gear/regular');
+
+    return media;
+  }
+}
--- app/twitter/index.mjs
+++ app/twitter/index.mjs
@@ -1,9 +1,11 @@
+import RegularGearTweet from "./generators/RegularGearTweet.mjs";
 import SchedulesTweet from "./generators/SchedulesTweet.mjs";
 import TwitterManager from "./TwitterManager.mjs"
 
 export function defaultTwitterManager() {
   return new TwitterManager([
     new SchedulesTweet,
+    new RegularGearTweet,
   ]);
 }
 
