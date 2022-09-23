4e2987b31347882291709061d69a6ace23c9c4d1
Matt Isenhower <matt@isenhower.com>
2022-09-23 09:33:18 -0400

    Update schedules tweet for Splatfests

--- app/twitter/generators/SchedulesTweet.mjs
+++ app/twitter/generators/SchedulesTweet.mjs
@@ -1,6 +1,6 @@
 import TweetGenerator from "./TweetGenerator.mjs";
 import Media from "../Media.mjs";
-import { useAnarchyOpenSchedulesStore, useAnarchySeriesSchedulesStore, useRegularSchedulesStore } from "../../../src/stores/schedules.mjs";
+import { useAnarchyOpenSchedulesStore, useAnarchySeriesSchedulesStore, useRegularSchedulesStore, useSplatfestSchedulesStore } from "../../../src/stores/schedules.mjs";
 
 export default class SchedulesTweet extends TweetGenerator
 {
@@ -14,6 +14,7 @@ export default class SchedulesTweet extends TweetGenerator
       regular: useRegularSchedulesStore().activeSchedule,
       anarchySeries: useAnarchySeriesSchedulesStore().activeSchedule,
       anarchyOpen: useAnarchyOpenSchedulesStore().activeSchedule,
+      splatfest: useSplatfestSchedulesStore().activeSchedule,
     }
   }
 
@@ -28,6 +29,12 @@ export default class SchedulesTweet extends TweetGenerator
   async _getStatus() {
     let stages = await this.getStages();
 
+    if (stages.splatfest?.settings) {
+      let festStages = stages.splatfest.settings.vsStages;
+
+      return `Join the global Splatfest Battle on ${festStages[0].name} and ${festStages[1].name}! #splatfest #maprotation`;
+    }
+
     return `Splatoon 3 map rotation: Anarchy (Series) game mode: ${stages.anarchySeries.settings.vsRule.name}, Anarchy (Open) game mode: ${stages.anarchyOpen.settings.vsRule.name} #maprotation`;
   }
 
@@ -39,13 +46,21 @@ export default class SchedulesTweet extends TweetGenerator
     let stages = await this.getStages();
     let detail = s => `${s.settings.vsRule.name} on ${s.settings.vsStages[0].name} and ${s.settings.vsStages[1].name}`;
 
-    media.altText = [
-      'Splatoon 3 map rotation:',
-      '',
-      `Regular Battle: ${detail(stages.regular)}`,
-      `Anarchy Battle (Series): ${detail(stages.anarchySeries)}`,
-      `Anarchy Battle (Open): ${detail(stages.anarchyOpen)}`,
-    ].join('\n');
+    let lines = ['Splatoon 3 map rotation:\n'];
+
+    if (stages.splatfest?.settings) {
+      lines.push(...[
+        `Splatfest Battle: ${detail(stages.splatfest)}`,
+      ]);
+    } else {
+      lines.push(...[
+        `Regular Battle: ${detail(stages.regular)}`,
+        `Anarchy Battle (Series): ${detail(stages.anarchySeries)}`,
+        `Anarchy Battle (Open): ${detail(stages.anarchyOpen)}`,
+      ]);
+    }
+
+    media.altText = lines.join('\n');
 
     return media;
   }
