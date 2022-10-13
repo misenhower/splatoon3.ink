630e3c699528f0c13cd5b0f8df38fe3d63012a32
Matt Isenhower <matt@isenhower.com>
2022-10-12 17:57:59 -0700

    Fix screenshots

--- src/screenshots.js
+++ src/screenshots.js
@@ -1,11 +1,16 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import { initializeI18n } from './common/i18n'
+
 import App from './App.vue'
 import router from './router/screenshots'
 
+const i18n = initializeI18n();
+
 const app = createApp(App)
 
+app.use(i18n)
 app.use(createPinia())
 app.use(router)
 
