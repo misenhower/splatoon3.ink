0493b175ea3ff384cf435d5e05404016a5826190
Matt Isenhower <matt@isenhower.com>
2022-10-21 10:03:36 -0700

    Tweak cron schedule

--- app/cron.mjs
+++ app/cron.mjs
@@ -5,7 +5,7 @@ import { sendTweets } from "./twitter/index.mjs";
 
 export default function() {
   new CronJob('5,20,35,50 * * * *', warmCaches, null, true);
-  new CronJob('10 0,1,2,5,10,15,30,45 * * * *', async () => {
+  new CronJob('15 0,2,5,10,15,30,45 * * * *', async () => {
     await updateAll();
     await sendTweets();
   }, null, true);
