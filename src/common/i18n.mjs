8ecdb0e1e7e0df7930fe6ccd257ca86cd9f63381
Matt Isenhower <matt@isenhower.com>
2022-10-24 17:07:17 -0700

    Update time strings for the remaining languages

--- src/common/i18n.mjs
+++ src/common/i18n.mjs
@@ -37,6 +37,9 @@ export function initializeI18n() {
       fallbackLocale: 'en-US',
       messages: { ...languages },
       datetimeFormats: locales.reduce((result, locale) => ({ ...result, [locale.code]: datetimeFormats }), {}),
+      pluralRules: {
+        'ru-RU': ruPluralization,
+      },
     });
 
     // Listen for local storage changes
@@ -45,6 +48,24 @@ export function initializeI18n() {
   }
 
   return i18n;
+}
+
+// Adapted from: https://vue-i18n.intlify.dev/guide/essentials/pluralization.html#custom-pluralization
+function ruPluralization(choice, choicesLength, orgRule) {
+  if (choice === 0) {
+    return 0
+  }
+
+  const teen = choice > 10 && choice < 20
+  const endsWithOne = choice % 10 === 1
+  if (!teen && endsWithOne) {
+    return 1
+  }
+  if (!teen && choice % 10 >= 2 && choice % 10 <= 4) {
+    return 2
+  }
+
+  return choicesLength < 4 ? 2 : 3
 }
 
 function reload() {
