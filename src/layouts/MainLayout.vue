0d558e77c906cedddbb7e2477811d7658d1734a9
Matt Isenhower <matt@isenhower.com>
2022-12-12 17:34:48 -0800

    Tweak fediverse icon size

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -38,7 +38,7 @@
         </a>
         &ndash;
         <a href="https://splatoon3.ink/@splatoon3ink" target="_blank">
-          <img src="@/assets/img/fediverse-white.svg" width="20" height="20" class="inline" />
+          <img src="@/assets/img/fediverse-white.svg" width="14" height="14" class="inline m-[3px]" />
           <span>@splatoon3ink@splatoon3.ink</span>
         </a>
         &ndash;
