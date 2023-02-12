3ffc71e5b11fc67bba6766fa22cde17c67663d4b
Matt Isenhower <matt@isenhower.com>
2023-02-12 09:13:26 -0800

    Move X-Rank updaters into a separate, low-priority schedule

--- app/cron.mjs
+++ app/cron.mjs
@@ -1,12 +1,15 @@
 import { CronJob } from "cron";
-import { updateAll } from "./data/index.mjs";
+import { updatePrimary, updateLowPriority } from "./data/index.mjs";
 import { warmCaches } from "./splatnet/index.mjs";
 import { sendStatuses } from "./social/index.mjs";
 
 export default function() {
   new CronJob('5,20,35,50 * * * *', warmCaches, null, true);
   new CronJob('15 0,2,5,10,15,30,45 * * * *', async () => {
-    await updateAll();
+    await updatePrimary();
     await sendStatuses();
+  }, null, true);
+  new CronJob('1 * * * *', async () => {
+    await updateLowPriority();
   }, null, true);
 }
--- app/data/index.mjs
+++ app/data/index.mjs
@@ -16,21 +16,43 @@ function updaters() {
     tokens.EU && new FestivalUpdater('EU'),
     tokens.JP && new FestivalUpdater('JP'),
     tokens.AP && new FestivalUpdater('AP'),
-    new XRankUpdater('Tentatek', 'ATLANTIC'),
-    new XRankUpdater('Takoroka', 'PACIFIC'),
   ].filter(u => u);
 }
 
-export async function updateAll() {
-  console.info('Running all updaters...');
+function lowPriorityUpdaters() {
+  return [
+    new XRankUpdater('Tentatek', 'ATLANTIC'),
+    new XRankUpdater('Takoroka', 'PACIFIC'),
+  ];
+}
 
-  for (let updater of updaters()) {
+async function run(updaters) {
+  for (let updater of updaters) {
     try {
       await updater.updateIfNeeded();
     } catch (e) {
       console.error(e);
     }
   }
+}
 
-  console.info('Done running updaters');
+export async function updateAll() {
+  console.info('Running all updaters...');
+  await run([
+    ...updaters(),
+    ...lowPriorityUpdaters(),
+  ]);
+  console.info('Done running all updaters');
+}
+
+export async function updatePrimary() {
+  console.info('Running primary updaters...');
+  await run(updaters());
+  console.info('Done running primary updaters');
+}
+
+export async function updateLowPriority() {
+  console.info('Running low-priority updaters...');
+  await run(lowPriorityUpdaters());
+  console.info('Done running low-priority updaters');
 }
