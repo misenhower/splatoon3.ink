143d1874746a7f358eafa62ba8a5bffc7f74ad23
Inkception <marvineichler18@gmail.com>
2023-06-01 15:29:23 +0200

    Update webViewVersion and StageScheduleQuery

--- app/splatnet/SplatNet3Client.mjs
+++ app/splatnet/SplatNet3Client.mjs
@@ -7,7 +7,7 @@ export const SPLATNET3_WEB_SERVICE_ID = '4834290508791808';
 export default class SplatNet3Client
 {
   baseUrl = 'https://api.lp1.av5ja.srv.nintendo.net';
-  webViewVersion = '3.0.0-2857bc50';
+  webViewVersion = '4.0.0-e2ee936d';
   bulletToken = null;
 
   constructor(nsoClient, acceptLanguage = 'en-US') {
@@ -117,7 +117,7 @@ export default class SplatNet3Client
   // Specific queries
 
   getStageScheduleData() {
-    return this.getGraphQLPersistedQuery(1, '011e394c0e384d77a0701474c8c11a20');
+    return this.getGraphQLPersistedQuery(1, 'd1f062c14f74f758658b2703a5799002');
   }
 
   getStageRecordData() {
