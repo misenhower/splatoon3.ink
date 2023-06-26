ea1e6222065b0396e3a6b1c8c2489d68caea681c
Matt Isenhower <matt@isenhower.com>
2023-06-25 20:59:31 -0700

    Update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,10 +3,24 @@ module.exports = {
   "root": true,
   "extends": [
     "plugin:vue/vue3-essential",
-    "eslint:recommended"
+    "eslint:recommended",
   ],
+  "rules": {
+    // ESLint
+    "indent": ["warn", 2, { "SwitchCase": 1 }],
+    "comma-dangle": ["warn", "always-multiline"],
+
+    // Vue
+    "vue/multi-word-component-names": "off",
+  },
+  "globals": {
+    "__dirname": "readonly",
+    "process": "readonly",
+    "require": "readonly",
+    "module": "readonly",
+  },
   "env": {
-    "vue/setup-compiler-macros": true
+    "vue/setup-compiler-macros": true,
   },
   "parserOptions": {
     "ecmaVersion": 13,
